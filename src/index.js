5580196dd672e107979b78b5875e1eeb456652c7
Alexandr Katyshev <katyshev1@gmail.com>
2023-06-29 17:08:53 +0300

    adds vars and fonts

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import '@fontsource-variable/montserrat';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
 import './index.html';
 import './index.scss';
