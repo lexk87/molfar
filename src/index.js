f32fc1107c80fdecf4cec51c34a86e7eb9ee797c
Alexandr Katyshev <katyshev1@gmail.com>
2023-07-04 00:43:33 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import '@fontsource-variable/montserrat';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.html';
 import './index.scss';
