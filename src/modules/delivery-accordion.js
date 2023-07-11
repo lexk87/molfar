559201da1209c9b3f75d08915dd3a51fbdffa533
Alexandr Katyshev <katyshev1@gmail.com>
2023-07-11 14:48:17 +0300

    a lot of changes/fixes

--- src/modules/delivery-accordion.js
+++ src/modules/delivery-accordion.js
@@ -1,14 +1,16 @@
 const refs = {
-    accordion: document.querySelector('.delivery__item'),
-    accordionBox: document.querySelector('.delivery__content'),
-    accordionText: document.querySelector('.delivery__text'),
+    accordion: document.querySelectorAll('.delivery__subheading-wrapper'),
+    accordionArrow: document.querySelectorAll('.delivery__accordion-arrow'),
+    accordionBox: document.querySelectorAll('.delivery__content'),
+    accordionText: document.querySelectorAll('.delivery__text'),
 };
 
-const { accordion, accordionBox, accordionText } = refs;
+const { accordion, accordionArrow, accordionBox, accordionText } = refs;
 
-const accordionToggle = () => {
-    accordionBox.classList.toggle('delivery__content--is-hidden');
-    accordionText.classList.toggle('delivery__text--is-hidden');
-};
-
-accordion.addEventListener('click', accordionToggle);
+for (let i = 0; i < accordion.length; i += 1) {
+    accordion[i].addEventListener('click', () => {
+        accordionArrow[i].classList.toggle('is-opened');
+        accordionBox[i].classList.toggle('delivery__content--is-hidden');
+        accordionText[i].classList.toggle('delivery__text--is-hidden');
+    });
+}
